0c558c5fe19086962bae1605dc4e2f7701112061
PCB-faith-audi <audisafaith@gmail.com>
2025-08-05 01:06:02 +0300

    initial commit

---
+++ Intro.py
@@ -0,0 +1,40 @@
+# PLP = "Academy"
+# Student_Name = "Faith Audi"
+# Student_ID = 1001
+#  Student_Age = 20
+# Fees_Clearance = True
+
+# #output
+# print(Student_Name, Student_ID, Student_Age, Fees_Clearance)
+
+# # Lists
+# languages = ["Python", "Java", "C++", "Javascript"]
+# print(languages)
+
+# shopping_list = ["Milk", "Eggs", "Bread", "Butter"]
+# print(shopping_list)
+
+# #Tuples
+# products = ('XBox', 499.99, "Habibi", 23)
+# print(products)
+
+# #Sets
+# Set_A = {1,2,3,4,5}
+# Set_B = {4,5,6,7,8}
+# print(Set_A)
+# print(Set_B)
+
+# #Dictionaries
+# student_info = {"Name": "Faith Audi, "ID": 1001, "Age": 20, "Fees_Clearance": True}
+# print(student_info)
+
+# capital_city = {"Kenya": "Nairobi", "Nigeria": "Lagos", "South Africa": "Cape Town"}
+# print(capital_city)
+
+#Logical Operators
+a = 10
+b = 20
+c = 30
+print(a < b and b < c) # True
+print(a < b or b > c) # True
+print(not (a < b))    # False
